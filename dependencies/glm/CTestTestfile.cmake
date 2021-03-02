783d3d00d694c2a367963e9fcd663f10f83e6b72
LoghinVladDev <vlad.loghin00@gmail.com>
2021-03-02 16:36:33 +0200

    submodule fix

--- dependencies/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/vladl/CLionProjects/c-eng/dependencies/glm
-# Build directory: C:/Users/vladl/CLionProjects/c-eng/dependencies/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
