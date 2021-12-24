afab26a8c4959defd36a6593b1fdc66ab047d4ec
Loghin Vlad-Andrei <vlad.loghin00@gmail.com>
2021-12-24 04:54:12 +0200

    Deep Analysis of VkAPI start

---
+++ src/engineRework/render/API/vulkan/extension/VulkanExtension.cpp
@@ -0,0 +1,5 @@
+//
+// Created by loghin on 24.12.2021.
+//
+
+#include "VulkanExtension.hpp"
---
+++ src/engineRework/render/API/vulkan/extension/VulkanExtension.hpp
@@ -0,0 +1,19 @@
+//
+// Created by loghin on 24.12.2021.
+//
+
+#ifndef C_ENG_VULKANEXTENSION_HPP
+#define C_ENG_VULKANEXTENSION_HPP
+
+#include <Preprocess.hpp>
+#include <CDS/Object>
+
+namespace engine {
+
+    class C_ENG_CLASS ( VulkanExtension ) : public cds :: Object {
+
+    };
+
+}
+
+#endif //C_ENG_VULKANEXTENSION_HPP
